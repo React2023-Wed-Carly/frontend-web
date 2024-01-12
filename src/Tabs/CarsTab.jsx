5a9c39045dc20a25cc9f4df44cd577abede1eebb
jdudz <jdudzinska625@gmail.com>
2024-01-12 22:01:14 +0100

    Add page navigation

---
+++ src/Tabs/CarsTab.jsx
@@ -0,0 +1,13 @@
+// CarsTab.jsx
+import React from 'react';
+
+const CarsTab = () => {
+	return (
+		<div>
+			<h2>Cars Tab Content</h2>
+			<p>This is a placeholder for the Cars tab content.</p>
+		</div>
+	);
+};
+
+export default CarsTab;
